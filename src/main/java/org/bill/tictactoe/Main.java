04291c603e98f001cf2383f85fd9be8e6f31a62a
Bill Schofield <bill@billschofield.net>
2017-12-19 12:53:41 -0500

    Removed unused code from main

--- src/main/java/org/bill/tictactoe/Main.java
+++ src/main/java/org/bill/tictactoe/Main.java
@@ -1,14 +1,9 @@
 package org.bill.tictactoe;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Main {
 
     public static void main(String[] args) {
-
         Board board = new Board(System.out);
-        BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
         Game game = new Game(board);
         game.start();
     }
