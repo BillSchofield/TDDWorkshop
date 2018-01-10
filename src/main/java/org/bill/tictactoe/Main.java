13c0d3b5ea71fdc5db35283dcee35fc44fd2f8e1
Bill Schofield <bill@billschofield.net>
2018-01-10 10:05:48 -0600

    Reset Project to baseline

--- src/main/java/org/bill/tictactoe/Main.java
+++ src/main/java/org/bill/tictactoe/Main.java
@@ -3,8 +3,5 @@ package org.bill.tictactoe;
 public class Main {
 
     public static void main(String[] args) {
-        Board board = new Board(System.out);
-        Game game = new Game(board);
-        game.start();
     }
 }
